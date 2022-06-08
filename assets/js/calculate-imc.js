999d36b9f3466639d387d307dadb221ecc09659e
Aleff Junio Silva Lins Lira <dev.alefflira@gmail.com>
2022-06-08 11:15:25 -0300

    Exclusão, inclusão, alteração e atualização de arquivos js, css e html

---
+++ assets/js/calculate-imc.js
@@ -0,0 +1,46 @@
+var pacientes = document.querySelectorAll('.paciente');
+
+// Loop
+for (var i = 0; i < pacientes.length; i++) {
+  //Variáveis
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector('.info-peso');
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector('.info-altura');
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector('.info-imc');
+
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+
+  // Condicionais
+  if (peso <= 0 || peso >= 1000) {
+    console.log('Peso inválido!');
+    pesoEhValido = false;
+    tdImc.textContent = 'Peso inválido';
+    paciente.classList.add('dado-invalido');
+  }
+
+  if (altura <= 0 || altura >= 3.0) {
+    console.log('Altura inválida!');
+    alturaEhValida = false;
+    tdImc.textContent = 'Altura inválida';
+    paciente.classList.add('dado-invalido');
+  }
+
+  if (alturaEhValida && pesoEhValido) {
+    var imc = calculaImc(peso, altura);
+    tdImc.textContent = imc;
+  }
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0;
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
+
+var botaoAdicionar = document.querySelector('#adicionar-paciente');
